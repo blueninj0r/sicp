6bb9ae9514c253161896b5b688e2296a67693062
Chris McCluskey <christopher.i.mccluskey@gmail.com>
2013-09-14 11:34:27 +0100

    Exercise 7 solution.
    Seems to work okay. Maybe a bit innacurate for very large numbers.
    Going to try an alternative I saw online...

--- ex_1_7.rkt
+++ ex_1_7.rkt
@@ -1,10 +1,11 @@
 #! /usr/bin/env racket
 #lang racket
 
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
+(define (sqrt-iter guess prev x)
+  (if (good-enough? guess prev x)
       guess
       (sqrt-iter (improve guess x)
+                 guess
                  x)))
 
 (define (improve guess x)
@@ -13,12 +14,16 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
+(define (good-enough? guess prev x)
+  (and (< (/ guess prev) 1.01)
+       (> (/ guess prev) .99)))
 
 (define (square x) (* x x))
 
 (define (sqrt x)
-  (sqrt-iter 1.0 x))
+  (sqrt-iter 1.0 0.0 x))
 
 (sqrt 9)
+(sqrt 0.0001)
+(sqrt 1000000000000)
+(sqrt 10000000000000)
